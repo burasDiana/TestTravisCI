37e649cf817fdf0456c1c6ece0ff1267e1d90c23
Diana Buras <burasdiana@hotmail.com>
2018-10-04 11:38:53 +0200

    [add] method in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Grade grade = new Grade();
+        System.out.println(grade.convertGrade('a'));
     }
-    public class Grade{
+    public static class Grade{
          int convertGrade(char c) {
             if(c == 'a')
                 return 12;
