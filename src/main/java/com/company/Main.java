ddac32b7c00581d855d61c5e63cb7e3125fb9b46
Diana Buras <burasdiana@hotmail.com>
2018-10-06 00:06:33 +0200

    [add] method to check conversion

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,13 +6,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Grade grade = new Grade();
-        System.out.println(grade.convertGrade('a'));
-    }
-    public static class Grade{
-         int convertGrade(char c) {
-            if(c == 'a')
-                return 12;
-            return -5;
+        grade.convertGrade(grade);
+        if(grade.GetValue_Danish() >= -3 ){
+            System.out.println(grade.GetValue_Danish());
+        }
+        else if(grade.GetValue_English() != null){
+            System.out.println(grade.GetValue_English());
         }
     }
 }
