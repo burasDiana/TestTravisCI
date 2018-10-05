fa81cd3f366f95ab170edace09677901d8bbc078
Diana Buras <burasdiana@hotmail.com>
2018-10-06 00:05:46 +0200

    [refactor] Grade class

--- src/main/java/com/company/Grade.java
+++ src/main/java/com/company/Grade.java
@@ -1,9 +1,33 @@
 package com.company;
 
 public class Grade {
-    public int convertGrade(char c) {
-        if(c == 'a')
-            return 12;
-        return -5;
+    enum Type{Danish,English}
+    private String value;
+    private int value_Danish;
+    private String  value_English;
+    private String type;
+    private String chars = "ABCDEF";
+
+    public String getType(String value){
+        if(chars.contains(value)){
+           return Type.English.toString();
+        }
+        return Type.Danish.toString();
+    }
+    public void convertGrade(Grade g) {
+        g.getType(g.value);
+        if(g.type == Type.English.toString() && chars.contains(g.value)){
+            g.value_English = g.value;
+    }
+        g.value_Danish = Integer.parseInt(g.value);
+    }
+    public void Set_Value(String value){
+        this.value = value;
+    }
+    public int GetValue_Danish(){
+        return value_Danish;
+    }
+    public String GetValue_English(){
+        return value_English;
     }
 }
